f800bb527e4975fab7a4abc46fd2228d81e71d94
macefang <macefang@gmail.com>
2018-03-01 06:22:01 +0700

    Try to adjust the port environment

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const dbConfig = require('./config/database');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //load routes
 const projectRoute = require('./apps/project/project.route');
