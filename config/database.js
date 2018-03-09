6e3cbf976ce44c8733a18e2d6c344846accad802
bthieu <bthieu@tma.com.vn>
2018-03-09 09:36:37 +0700

    Restructure the demo app

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const databaseConfig = {
-  mongoURI: 'mongodb://localhost/task-tracker-dev'  
+  //mongoURI: 'mongodb://localhost/task-tracker-dev'  
+  mongoURI: 'mongodb://test:test@ds153198.mlab.com:53198/ov-task-tracker'
 };
 if(process.env.NODE_ENV === 'production') {
   databaseConfig.mongoURI = 'mongodb://test:test@ds153198.mlab.com:53198/ov-task-tracker';
